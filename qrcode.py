8c4523dbd188406ae40fa1be63ab5fb6befde10d
aginga <brianaginga35@gmail.com>
2022-10-11 10:29:54 +0300

    newcode

--- qrcode.py
+++ qrcode.py
@@ -1,1 +1,15 @@
+import qrcode
 
+qr = qrcode.QRCode(
+    version=1,
+    box_size=15,
+    border=5
+)
+
+data = 'Lets make the change now'
+
+qr.add_data(data)
+qr.make(fit=True)
+img = qr.make_image(fill='black', black_color='white')
+
+img.save('now.png')
